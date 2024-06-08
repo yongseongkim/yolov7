b1f49b84be736849f8a114cb335e95ab35701e83
yongseongkim <yongseongkimm@gmail.com>
2024-06-08 19:45:55 +0900

    use SAM

--- blurpp.py
+++ blurpp.py
@@ -4,9 +4,9 @@ import os
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
-import pathlib as Path
-from torchvision.models.detection import maskrcnn_resnet50_fpn_v2
+from segment_anything import sam_model_registry, SamPredictor
 from torchvision.transforms import functional as F
 
 import dwnls3imgs
@@ -27,9 +27,7 @@ def get_person_masks(mask_rcnn, image, threshold=0.5):
     return masks
 
 
-def blur_people_in_image(mask_rcnn, img):
-    masks = get_person_masks(mask_rcnn, img)
-
+def blur_people_with_masks(img, masks):
     blurred_img = img.copy()
     for mask in masks:
         total_blurred_img = cv2.GaussianBlur(img, (0, 0), 4)
@@ -63,10 +61,25 @@ def add_suffix_to_filename(filename, suffix):
     return new_filename
 
 
-def blurpp(s3clinet, mask_rcnn, key, boxes):
-    output_dir = f'./runs/blurred'
-    img_path = dwnls3imgs.download_obj(s3client,
-                                       'scc-prod-accessibility-images', key, output_dir)
+def blurpp_with_sam(sam, img_path, key, boxes, output_dir):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    predictor = SamPredictor(sam)
+    predictor.set_image(img)
+    output_img = img.copy()
+    print(f'Blurring {len(boxes)} people in the image({key}).')
+    for box in boxes:
+        x1, y1, x2, y2 = box['x1'], box['y1'], box['x2'], box['y2']
+        masks, _, _ = predictor.predict(box=np.array([x1, y1, x2, y2]), point_coords=None, point_labels=None)
+        output_img = blur_people_with_masks(output_img, masks)
+    print(f'Blurring done.', end='\n')
+    show_comparison(img, output_img)
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f'{output_dir}/{add_suffix_to_filename(key, "_b")}', output_img)
+
+
+def blurpp(mask_rcnn, img_path, key, boxes, output_dir):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -75,21 +88,21 @@ def blurpp(s3clinet, mask_rcnn, key, boxes):
     print(f'Blurring {len(boxes)} people in the image({key}).')
 
     if not boxes:
-        output_img = blur_people_in_image(mask_rcnn, img)
+        masks = get_person_masks(mask_rcnn, img)
+        output_img = blur_people_with_masks(img, masks)
     else:
         for box in boxes:
             box = {key: int(value) for key, value in box.items()}
             x1, y1, x2, y2 = box['x1'], box['y1'], box['x2'], box['y2']
             partial_img = img[y1:y2, x1:x2:]
-            partial_blurred_img = blur_people_in_image(mask_rcnn, partial_img)
+            masks = get_person_masks(mask_rcnn, img)
+            partial_blurred_img = blur_people_with_masks(partial_img, masks)
             output_img[y1:y2, x1:x2, :] = partial_blurred_img
             # show_comparison(img, partial_img)
-    # os.remove(img_path)
-    output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
-    cv2.imwrite(
-        f'{output_dir}/{add_suffix_to_filename(key, "_b")}', output_img)
-    print(f'Blurring done.', end='\n')
     # show_comparison(img, output_img)
+    print(f'Blurring done.', end='\n')
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f'{output_dir}/{add_suffix_to_filename(key, "_b")}', output_img)
 
 
 if __name__ == '__main__':
@@ -98,11 +111,20 @@ if __name__ == '__main__':
         '--source', default='runs/detected/result.json', help='json path for person boxes')
     opt = parser.parse_args()
 
-    mask_rcnn = maskrcnn_resnet50_fpn_v2(pretrained=True)
-    mask_rcnn.eval()
+    # model = maskrcnn_resnet50_fpn_v2(pretrained=True)
+    model = sam_model_registry['vit_b'](checkpoint="sam_vit_b_01ec64.pth")
+    model.eval()
+
     with open(opt.source) as src:
         boxes = json.load(src)
         print(f'Load boxes from {opt.source}, {len(boxes)} found.')
-        s3client = dwnls3imgs.get_vault_session('scc')
+        client = dwnls3imgs.get_vault_session('scc')
+        idx = 0
+        output_dir = f'./runs/blurred'
         for (k, v) in boxes.items():
-            blurpp(s3client, mask_rcnn, k, v)
+            img_path = dwnls3imgs.download_obj(client, 'scc-prod-accessibility-images', k, output_dir)
+            blurpp_with_sam(model, img_path, k, v, output_dir)
+            # blurpp(model, img_path, k, v, output_dir)
+            idx += 1
+            if idx > 10:
+                break
